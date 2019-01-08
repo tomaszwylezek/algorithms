472972d2d703f87e331cbaaa1ab054a55ceead3f
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-08 15:47:05 +0100

    Palindrome

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str === str.split("").reverse().join('');
+}
 
 module.exports = palindrome;
+
+// Solution #2
+//
+// function palindrome(str) {
+//   return str
+//     .split("")
+//     .every((char, i) => char === str[str.length - i - 1]);
+// }
