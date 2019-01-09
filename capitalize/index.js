3445f8b6802bfcf97c0b4e857aced4564848f104
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-09 15:32:18 +0100

    Capitalize

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,26 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map(value => value[0].toUpperCase() + value.slice(1))
+    .join(" ");
+}
 
 module.exports = capitalize;
+
+// Solution #2
+//
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+//
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
