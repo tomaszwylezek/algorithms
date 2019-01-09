2cae728f5a7453ecc3877da8f8577a1795092914
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-09 12:56:46 +0100

    Reverse int

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const sign = Math.sign(n);
+
+  return (
+    sign *
+    Math.abs(n)
+      .toString()
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
 
 module.exports = reverseInt;
