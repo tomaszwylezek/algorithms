f6431b6e497d337bb1df062a69f6c97062715ea8
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-09 14:15:56 +0100

    Anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function cleanString(string) {
+  return string
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+}
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
 
 module.exports = anagrams;
+
+// Solution #2
+
+// function buildCharMap(string) {
+//   const charMap = {};
+//
+//   string
+//     .replace(/[^\w]/g, "")
+//     .toLowerCase()
+//     .split("")
+//     .forEach(value => {
+//       charMap[value] = charMap[value] + 1 || 1;
+//     });
+//
+//   return charMap;
+// }
+//
+// function anagrams(stringA, stringB) {
+//   const stringAMap = buildCharMap(stringA);
+//   const stringBMap = buildCharMap(stringB);
+//
+//   return (
+//     Object.keys(stringAMap).length === Object.keys(stringBMap).length &&
+//     Object.keys(stringBMap).every(value => {
+//       return stringAMap[value] === stringBMap[value];
+//     })
+//   );
+// }
