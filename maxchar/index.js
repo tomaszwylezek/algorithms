d145135602a18cb16f3b028ae47c831a9a8acc3e
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-09 13:15:19 +0100

    Maxchar

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  str.split("").forEach(char => {
+    charMap[char] = charMap[char]++ || 1;
+  });
+
+  let mostChars;
+
+  Object.keys(charMap).forEach(char => {
+    if (charMap[char] > (charMap[mostChars] || 0)) {
+      mostChars = char;
+    }
+  });
+
+  return mostChars;
+}
 
 module.exports = maxChar;
