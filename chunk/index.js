4de797543214451eb8b96499a76e949f51d22f4d
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-09 13:43:48 +0100

    Chunk

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
+
+// Solution #2
+
+// function chunk(array, size) {
+//   const chunked = [];
+//
+//   array.forEach(value => {
+//     const lastArray = chunked[chunked.length - 1];
+//
+//     if (!lastArray || lastArray.length === size) {
+//       chunked.push([value]);
+//     } else {
+//       lastArray.push(value);
+//     }
+//   });
+//
+//   return chunked;
+// }
