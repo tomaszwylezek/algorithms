250a4462ef5262c4e1818f7022f9f11512e2d98f
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-10 18:50:08 +0100

    Pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,55 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  for (let i = 0; i < n; i++) {
+    const hashes = new Array(2 * i + 1).fill("#").join("");
+    const spaces = new Array(n - (i + 1)).fill(" ").join("");
+    console.log(`${spaces}${hashes}${spaces}`);
+  }
+}
 
 module.exports = pyramid;
+
+// Solution #2
+//
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// Solution #3
+//
+// function pyramid(n, row = 0, level = "") {
+//   if (n === row) {
+//     return;
+//   }
+//
+//   if (level.length === 2 * n - 1) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+//
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//
+//   let add;
+//
+//   if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//     add = "#";
+//   } else {
+//     add = " ";
+//   }
+//
+//   pyramid(n, row, level + add);
+// }
