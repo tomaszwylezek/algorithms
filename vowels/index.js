60e44aec3bcdddcd2228abbf91c113ba7301e824
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-23 11:54:39 +0100

    Vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,38 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ?matches.length : 0;
+}
 
 module.exports = vowels;
+
+// Solution #2
+//
+// function vowels(str) {
+//   let count = 0;
+//
+//   const checker = "aeiou";
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// Solution #3
+//
+// const map = ["a", "e", "i", "o", "u"];
+//
+// function vowels(str) {
+//   let amount = 0;
+//   str.split("").forEach(letter => {
+//     if (map.includes(letter.toLowerCase())) {
+//       amount++;
+//     }
+//   });
+//   return amount;
+// }
